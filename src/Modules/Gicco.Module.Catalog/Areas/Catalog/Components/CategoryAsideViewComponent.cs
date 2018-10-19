368d7b90550a53d005facbb42f5e504450cb1574
tham davies <thamdv96@gmail.com>
2018-10-19 14:25:00 +0700

    Xong category sidebar

---
+++ src/Modules/Gicco.Module.Catalog/Areas/Catalog/Components/CategoryAsideViewComponent.cs
@@ -0,0 +1,54 @@
+﻿using Gicco.Infrastructure.Data;
+using Gicco.Infrastructure.Web;
+using Gicco.Module.Catalog.Models;
+using Gicco.Module.Catalog.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Gicco.Module.Catalog.Areas.Catalog.Components
+{
+    public class CategoryAsideViewComponent : ViewComponent
+    {
+        private readonly IRepository<Category> _categoryRepository;
+
+        public CategoryAsideViewComponent(IRepository<Category> categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categories = _categoryRepository.Query().Where(x => !x.IsDeleted && !x.IncludeInMenu).ToList();
+
+            var categoryMenuItems = new List<CategoryMenuItem>();
+            var topCategories = categories.Where(x => !x.ParentId.HasValue).OrderByDescending(x => x.DisplayOrder);
+            foreach (var category in topCategories)
+            {
+                var categoryMenuItem = Map(category);
+                categoryMenuItems.Add(categoryMenuItem);
+            }
+
+            return View(this.GetViewPath(), categoryMenuItems);
+        }
+
+        private CategoryMenuItem Map(Category category)
+        {
+            var categoryMenuItem = new CategoryMenuItem
+            {
+                Id = category.Id,
+                Name = category.Name,
+                Slug = category.Slug
+            };
+
+            var childCategories = category.Children;
+            foreach (var childCategory in childCategories.OrderByDescending(x => x.DisplayOrder))
+            {
+                var childCategoryMenuItem = Map(childCategory);
+                categoryMenuItem.AddChildItem(childCategoryMenuItem);
+            }
+
+            return categoryMenuItem;
+        }
+    }
+}

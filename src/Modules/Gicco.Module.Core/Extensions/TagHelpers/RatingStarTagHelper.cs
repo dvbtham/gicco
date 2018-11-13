9cb04df83e3a358668b168523badc34131d86b0e
tham davies <thamdv96@gmail.com>
2018-11-13 23:01:10 +0700

    Xem ảnh sản phẩm 1 cách chi tiết hơn

--- src/Modules/Gicco.Module.Core/Extensions/TagHelpers/RatingStarTagHelper.cs
+++ src/Modules/Gicco.Module.Core/Extensions/TagHelpers/RatingStarTagHelper.cs
@@ -9,12 +9,18 @@ namespace Gicco.Module.Core.Extensions.TagHelpers
         private const string RatingStarAttributeName = "gicco-rating-star";
 
         [HtmlAttributeName("rating-avg")]
-        public double? RatingAverage { get; set; }
+        public double? RatingAverage { get; set; } = double.MinValue;
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
-            if (!RatingAverage.HasValue)
+            if (RatingAverage == null)
+            {
+                for (int i = 1; i <= 5; i++)
+                {
+                    output.Content.AppendHtml("<i class='fa fa-star-o'></i>");
+                }
                 return;
+            }
 
             int unstared = 5;
 
